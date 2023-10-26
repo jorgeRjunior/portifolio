af4a9973a7c63d3c2f8b74d998e78f96b4926b8a
Jorge Ruediger Junior <90164297+jorgeRjunior@users.noreply.github.com>
2023-10-26 18:15:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import './App.css';
-import Header from './componentes/Header';
-import Profile from './componentes/Profile';
-import bgImage from './componentes/img/OIG.jpeg';
+import "./App.css";
+import Header from "./componentes/Header";
+import Profile from "./componentes/Profile";
+import bgImage from "./componentes/img/OIG.jpeg";
 
 function App() {
   return (
-      <>
+    <>
       <div>
-      <Header />
-      <Profile />
-      <div className='absolute left-0 top-0 -z-10 h-[100vh] w-[100vw] blur-[1px] opacity-80' style={{ backgroundImage: `url(${bgImage})` }}></div>
+        <Header />
+        <Profile />
       </div>
-      
-      </>
+
+      <div
+        className="absolute left-0 top-0 -z-10 h-full w-screen bg-fixed bg-cover-full blur-[0.1px] opacity-90"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      ></div>
+    </>
   );
 }
 
