f26a4439bf17e483cc449ff7c61a77551bd56256
Jorge Ruediger Junior <90164297+jorgeRjunior@users.noreply.github.com>
2023-10-27 08:15:17 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       </div>
 
       <div
-        className="absolute left-0 top-0 -z-10 h-full w-screen bg-fixed bg-cover-full blur-[0.1px] opacity-90"
+        className="fixed top-0 -z-10 h-full w-screen bg-fixed bg-cover-full blur-[0.1px] opacity-90"
         style={{ backgroundImage: `url(${bgImage})` }}
       ></div>
     </>
